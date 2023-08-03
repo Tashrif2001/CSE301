9b22bb4516c91e686ebfbd280da51601bfd7fa9c
Tashrif2001 <117228564+Tashrif2001@users.noreply.github.com>
2023-08-03 23:58:25 +0600

    Update activity.cpp

--- activity.cpp
+++ activity.cpp
@@ -1,43 +1,62 @@
-#include <bits/stdc++.h>
+#include<bits/stdc++.h>
 using namespace std;
+#define mx 100
 
-struct activity {
+struct act{
     string name;
-    int start_t;
-    int end_t;
+    int start;
+    int finish;
 };
 
-bool compare(activity a, activity b) {
-    return a.end_t < b.end_t;
+void b_sort(vector<act>& activities) {
+    for (int i = 0; i < activities.size() - 1; i++) {
+        for (int j = 0; j < activities.size() - 1; j++) {
+            if (activities[i].finish < activities[j].finish) {
+                struct act temp = activities[i];
+                activities[i] = activities[j];
+                activities[j] = temp;
+            }
+        }
+    }
 }
 
 int main() {
-    vector<activity> works;
+    freopen("test.txt", "r", stdin);
+    int num, t;
     string s;
-    int t;
-    for (int i = 0; i < 6; i++) {
-        cin >> s;
-        activity act;
-        act.name = s;
-        cin >> t;
-        act.start_t = t;
-        cin >> t;
-        act.end_t = t;
-        works.push_back(act);
-    }
-    sort(works.begin(), works.end(), compare);
 
-    vector<activity> sorted;
-    sorted.push_back(works[0]);
-    int lastEnd = works[0].end_t;
-    for (int i = 1; i < works.size(); i++) {
-        if (works[i].start_t >= lastEnd) {
-            sorted.push_back(works[i]);
-            lastEnd = works[i].end_t;
+    cout << "Enter number of activities: ";
+    cin >> num;
+
+    vector<act> activities;
+    vector<string> order;
+
+    for (int i = 0; i < num; i++) {
+        struct act act1;
+        cout << "Enter name of activity: ";
+        cin >> s;
+        act1.name = s;
+        cout << "Enter start time: ";
+        cin >> t;
+        act1.start = t;
+        cout << "Enter ending time: ";
+        cin >> t;
+        act1.finish = t;
+        activities.push_back(act1);
+    }
+
+    b_sort(activities);
+
+    int lastTime = activities[0].finish;
+    for (int i = 1; i < activities.size(); i++) {
+        if (activities[i].start >= lastTime) {
+            order.push_back(activities[i].name);
+            lastTime = activities[i].finish;
         }
     }
-    for (int i = 0; i < sorted.size(); i++) {
-        cout << sorted[i].name << endl;
-    }
+
+    for (int i = 0; i < order.size(); i++)
+        cout << order[i] << " ";
+
     return 0;
 }
