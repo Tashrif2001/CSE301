c8b4513af74e9e5c3cd971416de8bcc93f615938
Tashrif2001 <117228564+Tashrif2001@users.noreply.github.com>
2023-08-08 01:50:25 +0600

    Update knapsack.cpp

--- knapsack.cpp
+++ knapsack.cpp
@@ -1,30 +1,60 @@
-#include<iostream>
+#include<bits/stdc++.h>
 using namespace std;
 
 struct item{
     int value;
     int weight;
     double val_per_weight;
-}items[100];
+};
 
 bool compare(struct item item1, struct item item2)
 {
-    return item1.value_per_weight>item2.value_per_weight;
+    return item1.val_per_weight>item2.val_per_weight;
 }
-int benefit()
+void benefit(vector<item>&items, int w, int n)
 {
+    sort(items.begin(), items.end(), compare);
 
+    double total=0.0;
+    double curr_weight=0.0;
+    double x[n]={0};
+
+    for(int i=0; i<n; i++)
+    {
+        if(curr_weight + items[i].weight<=w)
+        {
+            total+=items[i].value;
+            curr_weight+=items[i].weight;
+            x[i]=items[i].weight;
+        }
+        else
+        {
+            double rem_weight = w-curr_weight;
+            total+=rem_weight*items[i].val_per_weight;
+            x[i]=rem_weight;
+            break;
+        }
+    }
+    for(int i=0; i<n; i++)
+    {
+        if(x[i])
+        cout<<x[i]<<"ml taken from "<<items[i].weight<<"ml"<<endl;
+    }
+    cout<<"Total Profit: "<<total<<endl;
 }
 int main()
 {
-    freopen("test.txt", "r", stdin);
-    int n;
+    //freopen("test.txt", "r", stdin);
+    int n, w;
     cin>>n;
+    vector<item>items(n);
     for(int i=0; i<n; i++)
     {
         cin>>items[i].value>>items[i].weight;
-        items[i].value_per_weight = (items[i].value/items[i].weight);
+        items[i].val_per_weight = (items[i].value/items[i].weight);
     }
-    int res = benefit();
+    cin>>w;
+    benefit(items, w, n);
+
     return 0;
 }
