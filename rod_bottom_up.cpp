cce5d8577f92d7564b06f61c3c9d914aa5a2d58d
Tashrif2001 <117228564+Tashrif2001@users.noreply.github.com>
2023-08-09 02:51:28 +0600

    Update rod_bottom_up.cpp

--- rod_bottom_up.cpp
+++ rod_bottom_up.cpp
@@ -1,33 +1,48 @@
-#include <bits/stdc++.h>
+#include<bits/stdc++.h>
 using namespace std;
 
-int rod_cut(vector<int>&price, int n)
+void print(vector<int>&s, int num)
 {
-    vector<int>dp(n+1);
+    while(num>0)
+    {
+        cout<<s[num]<<" ";
+        num-=s[num];
+    }
+    cout<<endl;
+}
+int rod_cut(vector<int>&price, int n, vector<int>&s)
+{
+    vector<int>dp(n+1, -1);
     dp[0]=0;
-    int q;
+
     for(int j=1; j<=n; j++)
     {
-        q=INT_MIN;
+        int q=INT_MIN;
         for(int i=0; i<j; i++)
         {
-            q = max(q, price[i]+dp[j-i-1]);
+            if(q<price[i]+dp[j-i-1])
+            {
+                q=price[i]+dp[j-i-1];
+                s[j]=i+1;
+            }
         }
         dp[j]=q;
     }
     return dp[n];
 }
-int main() {
-    //freopen("rod.txt", "r", stdin);
-    int n,num;
+int main()
+{
+    freopen("rod.txt", "r", stdin);
+    int num,n;
     cin>>num;
     vector<int>price(num);
+    vector<int>s(num, -1);
+
     for(int i=0; i<num; i++)
-    {
         cin>>price[i];
-    }
+
     cin>>n;
-    int res=rod_cut(price, n);
-    cout<<res;
+    cout<<rod_cut(price, n, s)<<endl;
+    print(s, n);
     return 0;
 }
